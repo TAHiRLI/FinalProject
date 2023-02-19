d99fb2cf91452c15741fe84c52b0219cc161e3f5
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-20 01:36:31 +0400

    doctor can create blog

--- MedlabApi/Medlab MVC_Uİ/Controllers/BlogController.cs
+++ MedlabApi/Medlab MVC_Uİ/Controllers/BlogController.cs
@@ -1,7 +1,9 @@
 ﻿using Medlab.Core.Entities;
 using Medlab.Core.Repositories;
+using Medlab_MVC_Uİ.Helpers;
 using Medlab_MVC_Uİ.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
 
@@ -11,12 +13,23 @@ namespace Medlab_MVC_Uİ.Controllers
     {
         private readonly IBlogRepostiory _blogRepostiory;
         private readonly IBlogCategoryRepository _blogCategoryRepository;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IWebHostEnvironment _env;
 
-        public BlogController(IBlogRepostiory blogRepostiory, IBlogCategoryRepository blogCategoryRepository)
+        public BlogController(IBlogRepostiory blogRepostiory, IBlogCategoryRepository blogCategoryRepository, UserManager<AppUser> userManager, IDoctorRepository doctorRepository, IWebHostEnvironment env)
         {
             this._blogRepostiory = blogRepostiory;
             this._blogCategoryRepository = blogCategoryRepository;
+            _userManager = userManager;
+            _doctorRepository = doctorRepository;
+            _env = env;
         }
+
+        //================================
+        // Index
+        //================================
+
         public IActionResult Index(int? BlogCategoryId = null)
         {
             BlogViewModel model = new BlogViewModel();
@@ -38,13 +51,17 @@ namespace Medlab_MVC_Uİ.Controllers
 
             }
 
-            model.BlogCategories = _blogCategoryRepository.GetAll(x => true).ToList();
+            model.BlogCategories = _blogCategoryRepository.GetAll(x => x.Blogs.Count> 0).ToList();
             model.RecentBlogs = _blogRepostiory.GetAll(x => true, "Doctor").OrderByDescending(x => x.CreatedAt).Take(3).ToList();
 
 
             ViewBag.BlogCategoryId = BlogCategoryId;
             return View(model);
         }
+
+        //================================
+        // Details
+        //================================
         public async Task<IActionResult>  Details(int id)
         {
 
@@ -54,13 +71,61 @@ namespace Medlab_MVC_Uİ.Controllers
             BlogDetailsViewModel model = new BlogDetailsViewModel
             {
                 Blog = blog,
-                BlogCategories = _blogCategoryRepository.GetAll(x => true).ToList(),
+                BlogCategories = _blogCategoryRepository.GetAll(x => x.Blogs.Count>0).ToList(),
                 RecentBlogs = _blogRepostiory.GetAll(x => true).OrderByDescending(x => x.CreatedAt).Take(3).ToList(),
             };
 
 
             return View(model);
 
+        }
+
+        //================================
+        // Create Blog
+        //================================
+
+        [Authorize(Roles ="Doctor")]
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _blogCategoryRepository.GetAll(x => true).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Doctor")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(BlogCreateViewModel BlogVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = _blogCategoryRepository.GetAll(x => true).ToList();
+                return View(BlogVm);
+            }
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user == null)
+                return NotFound();
+
+            var doctor = await _doctorRepository.GetAsync(x => x.Id == user.DoctorId, "Blogs");
+            if (doctor == null)
+                return NotFound();
+
+            Blog blog = new Blog
+            {
+                Title = BlogVm.Title,
+                BlogCategoryId = BlogVm.CategoryId,
+                ImageUrl = FileManager.Save(BlogVm.ImageFile, _env.WebRootPath ,"Assets/Uploads/Blogs",200),
+                DoctorId = doctor.Id,
+                Text = BlogVm.MainContent,
+                PrevText = BlogVm.PrevContent,
+                
+            };
+
+            doctor.Blogs.Add(blog);
+            _doctorRepository.Commit();
+
+            return RedirectToAction("profile", "account");
         }
     }
 }
---
+++ MedlabApi/Medlab MVC_Uİ/ViewModels/BlogCreateViewModel.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+
+namespace Medlab_MVC_Uİ.ViewModels
+{
+    public class BlogCreateViewModel
+    {
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
+        public IFormFile ImageFile { get; set; }
+        public string MainContent { get; set; }
+        public string PrevContent { get; set; }
+
+    }
+    public class BlogCreateVmValidator : AbstractValidator<BlogCreateViewModel>
+    {
+        public BlogCreateVmValidator()
+        {
+            RuleFor(x => x.Title).NotNull().NotEmpty()
+                .MaximumLength(100);
+            RuleFor(x => x.CategoryId).NotNull();
+            RuleFor(x => x.MainContent).NotNull().NotEmpty()
+                .MaximumLength(3000);
+            RuleFor(x => x.PrevContent).NotNull().NotEmpty()
+                .MaximumLength(300);
+
+            RuleFor(x => x).Custom((x, context) =>
+            {
+                if (x.ImageFile == null)
+                {
+                    context.AddFailure("ImageFile", "Main Image Is Required");
+
+                }
+                else if (x.ImageFile?.ContentType != "image/png" && x.ImageFile?.ContentType != "image/jpeg")
+                {
+                    context.AddFailure("ImageFile", "File Type must be jpeg or png");
+                }
+                else if (x.ImageFile?.Length > 2097152)
+                {
+                    context.AddFailure("ImageFile", "File size must be less than 2mb");
+                }
+
+
+
+            });
+        }
+    }
+}
